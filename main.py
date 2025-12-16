49335356b507c13cbdd5fefed8facc4ad367d233
qweasd371 <qrwqwr250@gmail.com>
2025-12-16 20:13:47 +0700

    v2.1

--- main.py
+++ main.py
@@ -28,25 +28,21 @@ class Main():
         self.root = tk.Tk()
         self.root.geometry(f"{self.width}x{self.height}")
 
-        # Создаём иформационные надписи
-        self.info_lable1 = tk.Label(self.root, text="Средне статистическое: 0.0000")
-        self.info_lable2 = tk.Label(self.root, text="Средне статистическое (без учёта нулей): 0.0000")
-        self.info_lable3 = tk.Label(self.root, text="Максимальное значение: 0.0000")
-        self.info_lable1.place(y=0, x=0)
-        self.info_lable2.place(y=18, x=0)
-        self.info_lable3.place(y=36, x=0)
+        # Создаём информационную надпись
+        self.info_lable = tk.Label(self.root, text="Максимальное значение: 0.0000")
+        self.info_lable.place(y=0, x=0)
 
         # Создаём поле ввода названия файла или путя
         self.file_name_entry = tk.Entry(self.root, width=50)
-        self.file_name_entry.place(y=55, x=0)
+        self.file_name_entry.place(y=30, x=0)
 
         # Создаём кнопку обновления названия файла или пути 
         self.file_name_update_button = tk.Button(self.root, text="Обновить название фаила.", command=self.update_file_name)
-        self.file_name_update_button.place(y=55, x=360)
+        self.file_name_update_button.place(y=30, x=360)
 
         # Создаём кнопку для запроса файла
         self.file_name_ask_path = tk.Button(self.root, text="Обзор", command=self.ask_path)
-        self.file_name_ask_path.place(y=85, x=360)
+        self.file_name_ask_path.place(y=60, x=360)
 
         # "Биндим" функции на различные действия
         self._focus_out_file_name_entry()
@@ -56,11 +52,11 @@ class Main():
 
         # Создаём кнопку загрузки и начала отрисовки 
         self.load_file_button = tk.Button(self.root, text="Загрузить файл", command=self.load_file)
-        self.load_file_button.place(y=75, x=0)
+        self.load_file_button.place(y=60, x=0)
 
         # Создаём кнопку остановки
         self.stop_button = tk.Button(self.root, text="Завершить отрисовку", command=self.stop)
-        self.stop_button.place(y=105, x=0)
+        self.stop_button.place(y=90, x=0)
         
         # Инициализируем график
         self.fig = Figure((self.width / self.dpi, self.height / self.dpi), dpi=self.dpi)
@@ -73,7 +69,7 @@ class Main():
         self.canvas.draw()
         toolbar = NavigationToolbar2Tk(self.canvas, self.root)
         toolbar.update()
-        self.canvas.get_tk_widget().pack(pady=135)
+        self.canvas.get_tk_widget().pack(pady=120)
         
         # Зацикливаем
         self.root.mainloop()
@@ -128,10 +124,8 @@ class Main():
         self.line_values = []
     
     def update_info_label(self, *argv):
-        "Обновление информациональных надписей."
-        self.info_lable1.config(text=f"Средне статистическое: {self.mean(self.list_data):.4f}")
-        self.info_lable2.config(text=f"Средне статистическое (без учёта нулей): {self.mean(self.list_data, True):.4f}")
-        self.info_lable3.config(text=f"Максимальное значение: {max(self.list_data) if self.list_data else 0:.4f}")
+        "Обновление информационной надписи."
+        self.info_lable.config(text=f"Максимальное значение: {max(self.list_data) if self.list_data else 0:.4f}")
     
     def update_file_name(self, *argv):
         "Обновление названия файла или путь."
@@ -155,19 +149,6 @@ class Main():
         if not self.file_name_entry.get().split():
             self.file_name_entry.delete(0, tk.END)
             self.file_name_entry.insert(tk.END, "Введите название фаила.")
-
-    @staticmethod
-    def mean(list_data, without_zero: bool = False):
-        "Средне арифметическое."
-        x = []
-        if without_zero:
-            x = [i for i in list_data if i]
-        else:
-            x = [i for i in list_data]
-        if x:
-            return sum(x) / len(x)
-        else:
-            return 0
         
 if __name__ == "__main__":
     Main()
